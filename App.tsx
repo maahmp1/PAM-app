a46c4478017e32ab46501991383cd50e2ea2fa3a
maahmp1 <marcelamartinspena@gmail.com>
2023-09-06 15:54:23 -0300

    add: att css

--- App.tsx
+++ App.tsx
@@ -1,15 +1,28 @@
-import {SafeAreaView, StatusBar } from 'react-native';
-import PowerUp from './PowerUp';
-import React from 'react';
-
+import { SafeAreaView, StatusBar } from "react-native";
+import PowerUp from "./PowerUp";
+import React from "react";
+import {
+  useFonts,
+  Fredoka_400Regular,
+  Fredoka_500Medium,
+  Fredoka_600SemiBold,
+} from "@expo-google-fonts/fredoka";
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    FRegular: Fredoka_400Regular,
+    FMedium: Fredoka_500Medium,
+    FBold: Fredoka_600SemiBold,
+  });
+
+  if (!fonteCarregada) {
+    return null;
+  }
+
   return (
-    <SafeAreaView >
-      <PowerUp/>
-      <StatusBar  />
+    <SafeAreaView>
+      <PowerUp />
+      <StatusBar />
     </SafeAreaView>
   );
 }
-
-
--- PowerUp/index.tsx
+++ PowerUp/index.tsx
@@ -1,19 +1,99 @@
 import React from "react";
-import { Image, StyleSheet, Text } from "react-native";
-import logo from "../assets/logo.png"
+import { Image, StyleSheet, Text, Dimensions, View } from "react-native";
+import logo from "../assets/logo.png";
+import topo from "../assets/topo.png";
 
-export default function PowerUp(){
+const width = Dimensions.get("screen").width;
 
-    return(
-        <Image source={logo} style={estilos.logo} />
-    ) 
+export default function PowerUp() {
+  return (
+    <>
+      <Image source={topo} style={estilos.topo} />
+      <Text style={estilos.titulo}>Detalhes do Carrinho</Text>
 
-   
+      <View style={estilos.carrinho}>
+        <Text style={estilos.nome}>Carrinho de Compras</Text>
+
+        <View style={estilos.teste}>
+          <View style={estilos.produto}>
+            <Image style={logo} source={logo}></Image>
+          </View>
+
+          <View>
+            <Text style={estilos.nomeProduto}>Power Up</Text>
+
+            <Text style={estilos.descricao}>
+              Uma creatina importada da Alemanha
+            </Text>
+          </View>
+        </View>
+        <Text style={estilos.preco}>R$80,00</Text>
+      </View>
+    </>
+  );
 }
 const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: (578 / 768) * width,
+  },
 
-    logo:{
-        width: "100%",
-        //  height: (578 / 768) * width,
-    }
-})
+  titulo: {
+    width: "100%",
+    fontFamily: "FMedium",
+    position: "absolute",
+    color: "white",
+    textAlign: "center",
+    fontSize: 24,
+    lineHeight: 26,
+    fontWeight: "bold",
+    padding: 16,
+  },
+
+  carrinho: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+
+  nome: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: "#464646",
+    fontFamily: "FBold",
+  },
+
+  teste: {
+    flexDirection: "row",
+  },
+
+  produto: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+
+  logo: {
+    width: 32,
+    height: 32,
+  },
+
+  nomeProduto: {
+    fontSize: 16,
+    fontFamily: "FBold",
+    lineHeight: 26,
+    marginLeft: 12,
+  },
+
+  descricao: {
+    color: "#A3A3A3",
+    fontFamily: "FRegular",
+
+    marginLeft: 10,
+  },
+
+  preco: {
+    fontSize: 26,
+    fontFamily: "FMedium",
+    color: "#2A9F85",
+    marginTop: 8,
+  },
+});
